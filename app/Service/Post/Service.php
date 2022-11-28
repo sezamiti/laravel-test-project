23e7a4c7aad0d31152823eff19f941e533ee4dfb
Sezam <dossumbekov@list.ru>
2022-11-29 05:57:27 +0600

    add:Service class in Laravel

--- app/Service/Post/Service.php
+++ app/Service/Post/Service.php
@@ -4,15 +4,26 @@
 namespace App\Service\Post;
 
 
+use App\Models\Post;
+
 class Service
 {
-    public function store()
+    public function store($data)
     {
+        $tags = $data['tags'];
+        unset($data['tags']);
 
+        $post= Post::create($data);
+
+        $post->tags()->attach($tags,  ['created_at' => new \DateTime('now')]);
     }
 
-    public function update()
+    public function update($post, $data)
     {
+        $tags = $data['tags'];
+        unset($data['tags']);
 
+        $post->update($data);
+        $post->tags()->sync($tags);
     }
 }
